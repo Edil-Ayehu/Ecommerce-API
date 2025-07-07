729ce404feb6f78935d56c2a435981263286d6c1
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-07-07 06:19:54 -0700

    advanced properties added to product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator"
+import { Type } from "class-transformer"
+import { IsArray, IsBoolean, IsDecimal, IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Max, Min } from "class-validator"
 
 export class CreateProductDto {
     @IsString()
@@ -10,7 +11,51 @@ export class CreateProductDto {
     @IsNotEmpty()
     price: number
 
+    @IsDecimal()
+    @IsPositive()
+    @IsOptional()
+    discountPercentage?: number
+
+    @IsString()
+    @IsNotEmpty()
+    thumbnailImage: string
+
+    @IsOptional()
+    @IsArray()
+    @IsString({each: true})
+    images?:string[] 
+
     @IsString()
     @IsOptional()
     description?: string
+
+    @IsOptional()
+    @IsInt()
+    @Min(1)
+    stock?: number = 1;
+
+    @IsString()
+    @IsNotEmpty()
+    category: string
+
+    @IsNumber({allowNaN: false, allowInfinity: false})
+    @Type(() => Number)
+    @Min(0)
+    @Max(5)
+    @IsOptional()
+    averageRating?: number = 0.0
+
+    @IsOptional()
+    @Min(0)
+    @IsInt()
+    numRatings?: number = 0;
+
+    @IsBoolean()
+    @IsOptional()
+    isFeatured?: boolean
+
+    @IsInt()
+    @Min(0)
+    @IsOptional()
+    salesCount?: number = 0
 }
--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -12,6 +12,37 @@ export class Product {
     @Column('decimal')
     price: number
 
+    @Column('decimal', { nullable: true })
+    discountPercentage?: number;
+
+    @Column()
+    thumbnailImage: string
+
+    @Column('text',{
+        array: true,
+        nullable: true,
+    })
+    images?: string[]
+
+    
+    @Column()
+    stock?: number = 1;
+
+    @Column()
+    category: string;
+
+    @Column('decimal', { default: 0.0 })
+    averageRating?: number;
+
+    @Column({ default: 0 })
+    numRatings: number;
+
+    @Column({ default: false })
+    isFeatured: boolean;
+
+    @Column({ default: 0 })
+    salesCount: number;
+
     @Column({
         type: 'text',
         nullable: true,
