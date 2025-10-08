976db6594a875173ae608d4f8328330313721313
edilayehu <edilayehu@eaglelionsystems.com>
2025-10-08 15:41:50 +0300

    response logging

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -13,17 +13,40 @@ export class LoggerMiddleware implements NestMiddleware {
     if (safeBody.password) safeBody.password = '[HIDDEN]';
     if (safeBody.token) safeBody.token = '[HIDDEN]';
 
+    // Store the original res.send method
+    const originalSend = res.send;
+
+    // Temporary variable to capture the response body
+    let responseBody: any;
+
+    // Override res.send to capture the response
+    res.send = function (body) {
+      responseBody = body;
+      return originalSend.call(this, body);
+    };
+
     // After response is sent
     res.on('finish', () => {
+
       const duration = Date.now() - start;
+
+      // Try parsing JSON response safely
+      let parsedResponse;
+      try {
+        parsedResponse = typeof responseBody === 'string' ? JSON.parse(responseBody) : responseBody;
+      } catch {
+        parsedResponse = responseBody;
+      }
+
       console.log('==============================');
       console.log(`[${req.method}] ${req.originalUrl}`);
       console.log('Status:', res.statusCode);
       console.log('Body:', safeBody);
+      console.log('Response:', parsedResponse);
       console.log(`Response Time: ${duration}ms`);
       console.log('==============================');
     });
 
-    next();
+    next(); // every middleware need to have this method at the end
   }
 }
